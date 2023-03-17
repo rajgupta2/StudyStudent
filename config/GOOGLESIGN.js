61c8f2b6b55197ddbdb5c2ceb6e1e8565958a660
Raj_Gupta <rajgupta.ckt22@gmail.com>
2023-03-17 16:28:11 +0530

    Managing Google user's Profile

--- config/GOOGLESIGN.js
+++ config/GOOGLESIGN.js
@@ -1,47 +1,63 @@
 const express = require("express");
-const Google=express();
+const Google = express();
 const passport = require("passport");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const DB=require("../Models/DB");
+const DB = require("../Models/DB");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_SIGNIN_CLIEND_ID,
-    clientSecret: process.env.GOOGLE_SIGNIN_CLIEND_SECRET,
-    callbackURL: process.env.GOOGLE_SIGNIN_CALLBACKURL
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    var Photo_URL=profile.photos[0].value;
-    const newuser=new DB.Colls_StdData({
-        googleId: profile.id,
-        Email:profile.emails[0].value,
-        Name: profile.displayName,
+  clientID: process.env.GOOGLE_SIGNIN_CLIEND_ID,
+  clientSecret: process.env.GOOGLE_SIGNIN_CLIEND_SECRET,
+  callbackURL: process.env.GOOGLE_SIGNIN_CALLBACKURL
+},
+  function (accessToken, refreshToken, profile, cb) {
+    var Photo_URL = profile.photos[0].value;
+    var filename = Photo_URL.substring(Photo_URL.lastIndexOf('/') + 1) + ".jpg";
+    const newuser = new DB.Colls_StdData({
+      googleId: profile.id,
+      Email: profile.emails[0].value,
+      Name: profile.displayName,
+      ProfilePicture: filename,
     });
-    DB.Colls_StdData.findOne({googleId: profile.id},function(err,user){
-        if(err){
-         return cb(err, user);
-        }else if(!user)
-        {
-            newuser.save(function(err){
-                if (err) {
-                    var errmsg="Sorry! Due to some technicle issue with google-sign,we are unable to registerd you.";
-                } 
-                return cb(err, user);
-            });
-        }else
-         return cb(err, user);
+    DB.Colls_StdData.findOne({ googleId: profile.id }, function (err, user) {
+      if (!user) {
+        //Downloading user's Profile Pic.
+        DownloadImage(Photo_URL);
+        newuser.save(function (err) {
+          if (err) {/*console.log(err);*/ }
+        });
+        return cb(err, newuser);
+      }
+      return cb(err, user);
     });
   }
 ));
 
 Google.get("/Login",
-  passport.authenticate('google', { scope: ["profile","email"] })
-  );
+  passport.authenticate('google', { scope: ["profile", "email"] })
+);
 
-Google.get("/Auth/Callback", 
-  passport.authenticate('google', { failureRedirect: '/Home/login' }),
-  function(req, res) {
+Google.get("/Auth/Callback",
+  passport.authenticate('google', { failureRedirect: '/Home/Login'}),
+  function (req, res) {
     // Successful authentication, redirect Student/Greetings.
     res.redirect('/Student/Greetings');
-});
+  });
 
-module.exports=Google;
+var DownloadImage=function(Photo_URL){
+  https.get(Photo_URL, (result) => {
+    //console.log(result.statusCode);
+    var filename = Photo_URL.substring(Photo_URL.lastIndexOf('/') + 1) + ".jpg";
+    var filePath = path.resolve("./Content//StudentProfileImage// ") + filename;
+    const writeStream = fs.createWriteStream(filePath);
+    result.pipe(writeStream);
+    writeStream.on("finish", () => {
+    writeStream.close();
+     //console.log("Download Completed!");
+   });
+ });
+}
+
+module.exports = Google;
